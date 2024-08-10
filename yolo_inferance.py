66397092ba182e26009b5fcf2ea45657c3041983
AhmedReda25300 <ahmedzalat25300@gmail.com>
2024-08-10 17:13:36 +0300

    training yolo5x for better results

--- yolo_inferance.py
+++ yolo_inferance.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolov8x.pt')
+model = YOLO('models/best.pt')
 results = model.predict('Input_videos/08fd33_4.mp4', save=True)
 print(results[0])
 print('______________________________________')
