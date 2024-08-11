1c0119523b8460b460420214ba8e2aa4156d7454
AhmedReda25300 <ahmedzalat25300@gmail.com>
2024-08-11 11:27:55 +0300

    working on players colors 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import read_video,save_video
 from trackers import Trackers
-
+import cv2
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
@@ -10,6 +10,18 @@ def main():
 
     Tracker = Trackers("models/best.pt")
     tracks = Tracker.get_objects_tracks(video_frames,read_from_stub=True,stub_path="stubs/tracks_stubs.pkl")
+
+    # save a cropped image for a player
+    for track_id, player in tracks["players"][0].items():
+        bbox = player["bbox"]
+        print(bbox)
+        frame = video_frames[0]
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        #save cropped image
+        cv2.imwrite(f"Output_videos/cropped_image.jpg", cropped_image)
+
+        break
+
 
     # draw output video
     ##draw output tracks
